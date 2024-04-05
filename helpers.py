6527c6fd42c2628e80e2db84cdda8cba0bbae1f2
Brandon Chin-A-Lien <bchinalien@outlook.com>
2024-04-05 04:58:28 +0200

    moved unification to helpers and added contains

--- helpers.py
+++ helpers.py
@@ -80,4 +80,37 @@ def free_vars(value: Union[PolyType, Context]) -> List[str]:
         case TypeQuantifier():
             return [var for var in free_vars(value.sigma) if var != value.a]
 
-raise Exception('Unknown argument passed to substitution')
+    raise Exception('Unknown argument passed to substitution')
+
+def unify(a: MonoType, b: MonoType) -> Substitution:
+    match a, b:
+        # Type Variables
+        case TypeVariable(), TypeVariable() if a.name == b.name:
+            return Substitution({})
+        case TypeVariable(), _ if contains(b, a):
+            raise Exception("Occurs check failed, cannot create infinite type")
+        case TypeVariable(), TypeVariable() :
+            # TODO: double check substition
+            Substitution({{a.a: b}})
+        case _, TypeVariable():
+            return unify(b, a)
+        
+        # Type Function Applications
+        case TypeFunctionApplication(), TypeFunctionApplication() if a.C != b.C:
+            raise Exception(f"Failed to unify different type functions: {a.C}, {b.C}")
+        case TypeFunctionApplication(), TypeFunctionApplication() if a.C == b.C:
+            S = Substitution({})
+            for t1, t2 in zip(a.mus, b.mus):
+                S = S.combine(unify(S(t1), S(t2)))
+            return S
+
+
+def contains(value: MonoType, type2) -> bool:
+    match value:
+        case TypeVariable(): # should always be a recurive call?
+            return value.a == type2.a 
+
+        case TypeFunctionApplication():
+            return any(contains(t, type2) for t in value.mus)
+
+    raise Exception('Unknown argument passed to contains')
